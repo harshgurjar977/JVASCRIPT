23d39d3bd97ea59a399a08a2b1246d53ca3ea860
harshgurjar977 <96569599+harshgurjar977@users.noreply.github.com>
2024-07-19 07:22:31 +0000

    conversion

---
+++ js/conversion.js
@@ -0,0 +1,21 @@
+// let someNum = 34
+
+// let stringNum = String(someNum)
+// console.log(stringNum)
+// console.log(typeof stringNum)
+
+
+/***********OPERATIONS************/
+
+console.log("1" + 2)   //12
+console.log(1 + "2")   //12
+console.log("1" + 2+2)  //122
+console.log(1 + 2 + "2") //32
+
+
+// PRIMITIVE
+// 7 types : string , number , boolean , null , undefined, symbol , BigInt
+
+
+// NON-PRIMITIVE
+// array , objects , functions
