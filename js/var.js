8e3800ffa69fe14aa59298eed692b335e011702e
harshgurjar977 <96569599+harshgurjar977@users.noreply.github.com>
2024-07-19 06:41:27 +0000

    new file add

---
+++ js/var.js
@@ -0,0 +1,16 @@
+const accountId = 14567 
+let accountEmail = "harsh977@gmail.com"
+var accountPass = "1234"
+accountCity = "jaipur"
+let accountState; //undefined
+
+//accountId = 2
+
+//let and var are same but the difference was about scope.let is used now days
+// these thing will give you error because const cant be change
+
+accountEmail = "harshgurjar977@gmail.com";
+accountPass = "4321";
+accountCity = "ratlam"
+
+console.table([accountId , accountEmail ,accountPass , accountCity]);
